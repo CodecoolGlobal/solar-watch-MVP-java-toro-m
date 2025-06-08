ee231bc793bcd3ef403aac86b6ead6dd23ab35c2
toro <toro.mark99@gmail.com>
2025-06-08 16:39:37 +0200

    controller endpoint created, service passed

--- solar-watch/src/main/java/com/codecool/solarwatch/controller/SolarWatchController.java
+++ solar-watch/src/main/java/com/codecool/solarwatch/controller/SolarWatchController.java
@@ -1,5 +1,6 @@
 package com.codecool.solarwatch.controller;
 
+import com.codecool.solarwatch.service.SunSetRiseService;
 import org.springframework.http.RequestEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,17 +9,21 @@ import java.time.LocalDate;
 
 public class SolarWatchController {
     private static final String DEFAULT_LOCATION="Budapest";
+    private SunSetRiseService sunSetRiseService;
 
+    public SolarWatchController(SunSetRiseService sunSetRiseService) {
+        this.sunSetRiseService = sunSetRiseService;
+    }
 
     @GetMapping("/sun")
-public RequestEntity getSunriseAndSunset(@RequestParam(required = false) String location, @RequestParam(required = false) LocalDate date) {
+public SunSetRiseReport getSunriseAndSunset(@RequestParam(required = false) String location, @RequestParam(required = false) LocalDate date) {
 if (location == null || location.equals("")) {
     location = DEFAULT_LOCATION;
 }
 if (date == null) {
     date = LocalDate.now();
 }
-//todo implement service and continue
+return sunSetRiseService.getSunSetRise(location, date);
 
     }
 
---
+++ solar-watch/src/main/java/com/codecool/solarwatch/service/SunSetRiseService.java
@@ -0,0 +1,15 @@
+package com.codecool.solarwatch.service;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+public class SunSetRiseService {
+    // todo get this off github
+    private static final String API_KEY = "3e0cd5534393786af5b3cf1eccf2f97b";
+
+
+    public SunSetRiseReport getSunSetRise(String location, LocalDate date) {
+    }
+}
