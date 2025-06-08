126b5ddcc6caca6868437702328223d064688724
toro <toro.mark99@gmail.com>
2025-06-08 17:28:31 +0200

    coordinates form locatin name aquired

---
+++ solar-watch/src/main/java/com/codecool/solarwatch/model/Coordinates.java
@@ -0,0 +1,4 @@
+package com.codecool.solarwatch.model;
+
+public record Coordinates(String name, double lat, double lon) {
+}
--- solar-watch/src/main/java/com/codecool/solarwatch/service/SunSetRiseService.java
+++ solar-watch/src/main/java/com/codecool/solarwatch/service/SunSetRiseService.java
@@ -1,7 +1,9 @@
 package com.codecool.solarwatch.service;
 
+import com.codecool.solarwatch.model.Coordinates;
 import com.codecool.solarwatch.model.SunSetRiseReport;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDate;
 
@@ -10,7 +12,16 @@ public class SunSetRiseService {
     // todo get this off github
     private static final String API_KEY = "3e0cd5534393786af5b3cf1eccf2f97b";
 
+    private final RestTemplate restTemplate;
+
+    public SunSetRiseService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     public SunSetRiseReport getSunSetRise(String location, LocalDate date) {
+String coordinatesFromLocationUrl= String.format("http://api.openweathermap.org/geo/1.0/direct?q=%s&appid=%s", location, API_KEY);
+
+Coordinates coordinates = restTemplate.getForObject(coordinatesFromLocationUrl, Coordinates.class);
+
     }
 }
