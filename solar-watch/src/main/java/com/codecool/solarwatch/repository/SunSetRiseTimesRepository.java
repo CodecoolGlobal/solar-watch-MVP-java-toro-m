533ed4df5af04ae2694e2dfc577be3aa332d9adc
toro <toro.mark99@gmail.com>
2025-06-22 22:50:34 +0200

    repository spring tag added

--- solar-watch/src/main/java/com/codecool/solarwatch/repository/SunSetRiseTimesRepository.java
+++ solar-watch/src/main/java/com/codecool/solarwatch/repository/SunSetRiseTimesRepository.java
@@ -2,6 +2,8 @@ package com.codecool.solarwatch.repository;
 
 import com.codecool.solarwatch.model.SunSetRiseTimesData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SunSetRiseTimesRepository extends JpaRepository<SunSetRiseTimesData, Long> {
 }
