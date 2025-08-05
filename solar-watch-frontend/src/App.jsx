24611ad10a3d99be547e3cb14b97ff1e83568612
toro <toro.mark99@gmail.com>
2025-08-05 12:05:04 +0200

    login page and routing created

--- solar-watch-frontend/src/App.jsx
+++ solar-watch-frontend/src/App.jsx
@@ -1,9 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
 
 function App() {
-
   return (
-    <>     </>
-  )
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+
+      </Routes>
+  );
 }
 
-export default App
+function AppWrapper() {
+  return (
+      <Router>
+        <App />
+      </Router>
+  );
+}
+
+export default AppWrapper;
