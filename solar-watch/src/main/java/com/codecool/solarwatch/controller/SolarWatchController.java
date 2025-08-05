913261b71b5fbcf8513486b7b45e6b57df6f8372
toro <toro.mark99@gmail.com>
2025-08-05 12:57:52 +0200

    /api added for mapping

--- solar-watch/src/main/java/com/codecool/solarwatch/controller/SolarWatchController.java
+++ solar-watch/src/main/java/com/codecool/solarwatch/controller/SolarWatchController.java
@@ -5,12 +5,14 @@ import com.codecool.solarwatch.model.SunSetRiseTimes;
 import com.codecool.solarwatch.model.SunSetRiseTimesData;
 import com.codecool.solarwatch.service.SunSetRiseService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 
 @RestController
+@RequestMapping("/api")
 public class SolarWatchController {
     private static final String DEFAULT_LOCATION = "Budapest";
     private SunSetRiseService sunSetRiseService;
