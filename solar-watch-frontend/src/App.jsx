0415a48d5b704da44bd89e98c42b82ad3ad5a7b5
toro <toro.mark99@gmail.com>
2025-08-05 13:18:46 +0200

    register page created, login page post fixed

--- solar-watch-frontend/src/App.jsx
+++ solar-watch-frontend/src/App.jsx
@@ -1,21 +1,22 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
-  return (
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-
-      </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/login" element={<LoginPage/>}/>
+            <Route path="/register" element={<RegisterPage/>}/>
+        </Routes>
+    );
 }
 
 function AppWrapper() {
-  return (
-      <Router>
-        <App />
-      </Router>
-  );
+    return (
+        <Router>
+            <App/>
+        </Router>
+    );
 }
 
 export default AppWrapper;
